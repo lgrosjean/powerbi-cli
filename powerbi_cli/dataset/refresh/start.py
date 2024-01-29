2101f2bc978a0086de6a83ec8c0fb2e2019879fa
Léo Grosjean <34337781+lgrosjean@users.noreply.github.com>
2024-01-29 10:50:12 +0100

    Feat/rename entrypoint (#11)
    * refacto: pbi to powerbi + auth docs
    
    * add: docker workflow + docs
    
    * fix: lint and refresh

--- powerbi_cli/dataset/refresh/start.py
+++ powerbi_cli/dataset/refresh/start.py
@@ -1,28 +1,34 @@
 import click
 
+from powerbi_cli.client import get_client
+
 
 @click.command()
+@click.option("-w", "--workspace", default=None)
+@click.option(
+    "--notify",
+    type=click.Choice(["MailOnCompletion", "MailOnFailure", "NoNotification"]),
+)
+@click.option(
+    "--type",
+    "type_",
+    type=click.Choice(
+        ["Automatic", "Calculate", "ClearValues", "DataOnly", "Defragment", "Full"]
+    ),
+)
+@click.argument("dataset")
 def start(
     dataset,
     workspace,
-    type,
+    type_,
     notify,
 ):
     """Triggers a refresh for the specified dataset from the specified workspace."""
+    pbi = get_client()
+
     endpoint = f"/datasets/{dataset}/refreshes"
     if workspace:
         endpoint = f"groups/{workspace}/" + endpoint
 
-    notify_option = (
-        "MailOnCompletion"
-        if notify == "always"
-        else "MailOnFailure"
-        if notify == "failyre"
-        else "NoNotification"
-    )
-
-    body = {"notifyOption": notify_option}
-
-    # res = client.post(endpoint)
-    # typer.echo(body)
-    # typer.echo(res.json())
+    dataset_ = pbi.dataset(dataset=dataset, group=workspace)
+    dataset_.refresh(notify_option=notify, type=type_)
--- powerbi_cli/main.py
+++ powerbi_cli/main.py
@@ -8,16 +8,16 @@ from .workspace import workspace
 
 
 @click.group()
-def pbi():
-    """Base PowerBI CLi entrypoint"""
+def powerbi():
+    """Base PowerBI CLI entrypoint"""
 
 
-pbi.add_command(dataset)
-pbi.add_command(workspace)
-pbi.add_command(auth)
-pbi.add_command(report)
-pbi.add_command(admin)
+powerbi.add_command(dataset)
+powerbi.add_command(workspace)
+powerbi.add_command(auth)
+powerbi.add_command(report)
+powerbi.add_command(admin)
 
 
 if __name__ == "__main__":
-    pbi()
+    powerbi()
