e2d7d86eafd98cc63839ff12f3697ce79215e731
Léo Grosjean <leo.grosjean@servier.com>
2024-01-29 10:20:26 +0100

    refacto: pbi to powerbi + auth docs

--- powerbi_cli/client.py
+++ powerbi_cli/client.py
@@ -3,19 +3,24 @@ from functools import cache
 import structlog
 from azure.identity import DefaultAzureCredential
 from pbipy import PowerBI
-from requests import Session
 
 SCOPE = "https://analysis.windows.net/powerbi/api/.default"
 
 
 class PBIClient(PowerBI):
-    def __init__(self, bearer_token: str, session: Session = None) -> None:
-        super().__init__(bearer_token, session)
-        self.logger = structlog.get_logger()
+    def __init__(self, bearer_token: str) -> None:
+        super().__init__(bearer_token)
+        self.logger = structlog.get_logger(self.__class__.__name__)
 
 
 def get_token() -> str:
-    """Returns Azure default token"""
+    """Returns Azure default token. The following credential types if
+    enabled will be tried, in order:
+    - EnvironmentCredential
+    - VisualStudioCodeCredential
+    - AzureCliCredential
+    - InteractiveBrowserCredential
+    """
     credentials = DefaultAzureCredential()
     access_token = credentials.get_token(SCOPE)
     token = access_token.token
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,13 @@
 from click.testing import CliRunner
 
-from powerbi_cli.main import pbi
+from powerbi_cli.main import powerbi
 
 
 def test_pbi():
     runner = CliRunner()
-    result = runner.invoke(pbi)
+    result = runner.invoke(powerbi)
     assert result.exit_code == 0
-    assert "Usage: pbi [OPTIONS] COMMAND [ARGS]..." in result.output
+    assert "Usage: powerbi [OPTIONS] COMMAND [ARGS]..." in result.output
     assert "admin" in result.output
     assert "auth" in result.output
     assert "dataset" in result.output
@@ -17,11 +17,11 @@ def test_pbi():
 
 def test_pbi_admin():
     runner = CliRunner()
-    result = runner.invoke(pbi, "admin")
+    result = runner.invoke(powerbi, "admin")
     assert result.exit_code == 0
 
 
 def test_pbi_auth():
     runner = CliRunner()
-    result = runner.invoke(pbi, "auth")
+    result = runner.invoke(powerbi, "auth")
     assert result.exit_code == 0
